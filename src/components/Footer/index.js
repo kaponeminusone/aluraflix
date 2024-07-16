5f63b6ec5d9fd0a979cdd53b1754aadafa90f3b4
kaponeminusone <nhenaoz@unicartagena.edu.co>
2024-07-16 18:39:16 -0500

    loading project

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.alura.com.br/">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+    </FooterBase>
+  );
+}
+
+export default Footer;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/logo.png';
+import Button from '../Button';
+import './Menu.css';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <Link to="/">
+                <img className="Logo" src={Logo} alt="Logo da AluraFlix" />
+            </Link>
+
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo vídeo
+            </Button>
+        </nav>
+    )
+}
+export default Menu;
